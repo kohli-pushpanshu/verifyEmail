3a7cb592a594febd012d28a0a63d518884c63052
kohli-pushpanshu <pushpanshugujjar@gmail.com>
2025-06-09 19:10:41 +0530

    Changes

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -1,7 +1,6 @@
 import { prisma } from 'lib/prisma';
 import { NextRequest, NextResponse } from 'next/server';
 import bcryptjs from 'bcryptjs';
-import { testPostgresConnection } from 'lib/dbCheck';
 import jwt from 'jsonwebtoken'
 
 
--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -1,10 +1,10 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import { testPostgresConnection } from 'lib/dbCheck';
 
 
 
 testPostgresConnection()
-export async function GET(request: NextRequest){
+export async function GET(){
     try {
         const Response=NextResponse.json({
             message: "Logout Successfully",
